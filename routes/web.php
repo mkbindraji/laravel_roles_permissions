abd221b4e36ee5c27ec046d5ab45d24e6cb66965
mkbindraji <mkbindra2003@gmail.com>
2024-09-23 08:03:20 +0530

    create_list_articales

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleConroller;
@@ -35,6 +36,14 @@ Route::middleware('auth')->group(function () {
     Route::post('/roles/{id}', [RoleConroller::class, 'update'])->name('roles.update');
     Route::delete('/roles', [RoleConroller::class, 'destroy'])->name('roles.destroy');
 
+
+    //Articles routes
+    Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    // Route::get('/roles/{id}/edit', [RoleConroller::class, 'edit'])->name('roles.edit');
+    // Route::post('/roles/{id}', [RoleConroller::class, 'update'])->name('roles.update');
+    // Route::delete('/roles', [RoleConroller::class, 'destroy'])->name('roles.destroy');
 
 });
 
